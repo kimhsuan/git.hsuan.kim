b8b6a359dc58ab21991c4e5feb7b73f0d95d5944
Hsuan <hsuan@hsuan.kim>
2022-03-05 13:22:40 +0000

    first commit

---
+++ certbot.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+action=${1-}
+
+domain=git.hsuan.kim
+
+function certonly() {
+  sudo mkdir -p /data/html/.well-known/acme-challenge
+  docker run -it --rm --name certbot \
+      -v "/data/letsencrypt:/etc/letsencrypt" \
+      -v "/data/html:/usr/local/openresty/nginx/html" \
+      -v "/var/log/letsencrypt:/var/log/letsencrypt" \
+      certbot/certbot certonly --webroot \
+      -w /usr/local/openresty/nginx/html -d ${domain} \
+      --agree-tos --register-unsafely-without-email
+  sudo cp -rf /etc/letsencrypt/archive/${domain} /data/ssl
+}
+
+function renew() {
+  docker run -it --rm --name certbot \
+      -v "/data/letsencrypt:/etc/letsencrypt" \
+      -v "/data/html:/usr/local/openresty/nginx/html" \
+      -v "/var/log/letsencrypt:/var/log/letsencrypt" \
+      certbot/certbot renew
+  sudo cp -rf /etc/letsencrypt/archive/${domain} /data/ssl
+}
+
+function main() {
+  case "${action}" in
+  certonly)
+    certonly
+    ;;
+  renew)
+    renew
+    ;;
+  *)
+    renew
+    ;;
+  esac
+}
+
+main "$@"
